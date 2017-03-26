d9f3f75a58aa3ba07cb5005009d8aae1b178702a
ibm_admin <ibm_admin@9.77.199.175>
2017-03-26 11:27:19 +0200

    k

--- CarPool/src/pool/runner/Runnerfile.java
+++ CarPool/src/pool/runner/Runnerfile.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/pool/feature")
+@CucumberOptions(features = "src/pool/feature")
 public class Runnerfile {
 
 }
